1e0fcbafd87445eb79e9fd151cc9f5842d3755cc
kvasudeva24 <vasudevakartik@gmail.com>
2025-11-15 17:48:10 -0600

    finished mathcing logic

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -105,3 +105,70 @@ void OrderBook::placeBidOrder(Order* order){
 }
 
 
+
+void OrderBook::walkBidBook(Order* order){
+    //two cases (it either crosses the spread and we have to process or it doesnt and we can insert)
+    if(order->getPrice() > best_bid){
+        placeBidOrder(order);
+    } else {
+        //order crosses the spread
+        Shares shares = order->getShares();
+        PriceList* cur_list = &bid_book[best_bid];
+        while(!bid_book.empty() && order->getPrice() <= best_bid && shares > 0){
+            
+            while(cur_list->getSize() > 0 && shares > 0){
+                Order* front = cur_list->remove_front();
+                shares = std::max(0.0, shares - front->getShares());
+                removeOrder(front->getId());
+            }
+
+            //there are two cases after we walk a price list
+            //1. the list still has orders and we have filled
+            //2. the list is empty and we MAY need to still fill
+            if(cur_list->getSize() == 0){
+                cur_list = nullptr;
+                bid_book.erase(best_bid);
+                if(!bid_book.empty()){
+                    auto it = bid_book.end();
+                    while(it == bid_book.end()){
+                        best_bid -= TICK_SIZE;
+                        it = bid_book.find(best_bid);
+                    }
+                    cur_list = &it->second;
+                } else {
+                    best_bid = __DBL_MIN__;
+                }
+            }
+        }
+        //dont need to do anything else if filled
+        if(shares > 0){
+            order->setShares(shares);
+            placeBidOrder(order);
+        }
+
+    }
+}
+
+void OrderBook::placeSellOrder(Order* order){
+
+    Price price = order->getPrice();
+    //we first have to (check if it is the best bid (ie the one that would get filled first)
+    best_ask = std::min(best_bid, price);
+
+    //now we can insert the order to the price level
+    //find if it exists
+    auto it = ask_book.find(price);
+
+    if(it == ask_book.end()){
+        ask_book.emplace(std::make_pair(price, PriceList()));
+        ask_book[price].add_order(order);
+    } else {
+        ask_book[price].add_order(order);
+    }
+
+    //insert into the order_map
+    order_map.insert(std::make_pair(order->getId(), order));
+    return;
+}
+
+
